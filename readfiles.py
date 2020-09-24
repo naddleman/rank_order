7960f860a82af5c1fd0cf2c14f30fd8bfba6a09e
naddleman <nik.addleman@gmail.com>
2020-09-23 17:26:10 -0700

    Add pairwise winrate function

--- readfiles.py
+++ readfiles.py
@@ -17,8 +17,34 @@ import pandas as pd
 sheets_xlsx = [fn for fn in os.listdir() if fn.endswith('.xlsx')]
 
 def valid_sin_xls_sheets():
-    sheets_xls  = [fn for fn in os.listdir() if fn.endswith('.xls')]
+    sheets_xls  = [fn for fn in os.listdir('./data/') if fn.endswith('.xls')]
     eight_sins = ['PA', 'PH', 'PJ', 'PM', 'PP', 'PC']
     only_7 = [sheet for sheet in sheets_xls if not sheet[:2] in eight_sins]
     return only_7
+
+def concatenate_excel_sheets(file_list, columns=7):
+    """Produces a single df with all the rank order data from many excel
+       spreadsheets. The first column is assumed to be irrelevant"""
+    dfs = []
+    for file_name in file_list:
+        dfs.append(pd.read_excel(os.path.join('data', file_name),
+                                 usecols=range(1, columns + 1)))
+    return pd.concat(dfs)
+
+def pairwise_winrates(df):
+    """Generates an n-by-n matrix of probabilities that <row> is ranked higher
+       than <column> by a player in the dataframe of rank orders"""
+    winrates = dict()
+    nrows = df.shape[0]
+    for col_1 in df.columns:
+        for col_2 in df.columns:
+            if col_1 == col_2:
+                winrates[(col_1, col_1)] = 0.5
+            else:
+                difference = df[col_1] - df[col_2]
+                count_wins = (difference < 0).value_counts()[True]
+                winrates[(col_1, col_2)] = count_wins / nrows
+    return winrates
+                
+
 
